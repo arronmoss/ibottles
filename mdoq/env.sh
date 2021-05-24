73a16e66133050c260042c1f568ab0f66545445a
Arron Moss <arron.moss@zero1.co.uk>
2021-05-24 09:17:36 +0100

    strip test

--- mdoq/env.sh
+++ mdoq/env.sh
@@ -3,14 +3,13 @@ cd ~/htdocs;
 mv pub/errors/local.xml.sample pub/errors/local.xml
 
 #paypal test
-php bin/magento config:set paypal/wpp/api_username arron.moss+sandboxbusiness_api1.zero1.co.uk
-php bin/magento config:set paypal/wpp/api_password JDDJ8PDDH6XATT2U
-php bin/magento config:set paypal/wpp/api_signature AN7RYZQ.31bZqTgC8c9AIBbiy4tkA3-oEyYqgcMiCqDxuTpGl8M-7Q0Q
-php bin/magento config:set paypal/general/business_account arron.moss@zero1.co.uk
-php bin/magento config:set paypal/wpp/sandbox_flag 1 
+bin/magento config:set paypal/wpp/api_username arron.moss+sandboxbusiness_api1.zero1.co.uk
+bin/magento config:set paypal/wpp/api_password JDDJ8PDDH6XATT2U
+bin/magento config:set paypal/wpp/api_signature AN7RYZQ.31bZqTgC8c9AIBbiy4tkA3-oEyYqgcMiCqDxuTpGl8M-7Q0Q
+bin/magento config:set paypal/general/business_account arron.moss@zero1.co.uk
+bin/magento config:set paypal/wpp/sandbox_flag 1 
 
-#php bin/magento config:set sagepaysuite/global/vendorname zero1ltd
-#php bin/magento config:set sagepaysuite/global/mode test
+bin/magento config:set --lock-config payment/stripe_payments_basic/stripe_mode test
 
 # Elastic
 #m2db_host=$(php -r '$env = include "./app/etc/env.php"; echo $env["db"]["connection"]["default"]["host"].PHP_EOL;');
