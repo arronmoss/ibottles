6774bd011ba5a80191a08320887d78e63a61ba5d
Craig <craig.guille@zero1.co.uk>
2021-08-26 10:26:04 +0000

    Upgrade to 2.4.3 push 43

---
+++ wp/wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1550067847;s:7:"authKey";s:64:",9,7ttvk=|$Wf|lmZZ77j$Tjm020sem{Zt0vKyY{{QM. ilKxl3X7VTKfj)};r7C";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:540;}
